ac3ffb92da299ae3eccc67853371bc244abfc352
Muhammed Mukthar <muktharmanaf28@gmail.com>
2022-10-08 18:43:58 +0530

    react app created creating navbar

---
+++ src/Components/NavBar/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function navbar() {
+  return (
+    <div className='navbar'>
+
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="netflixlogo" />
+<img src="" alt="" />
+
+    </div>
+  )
+}
+
+export default navbar
